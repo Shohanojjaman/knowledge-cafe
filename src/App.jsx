4391e1e02d56868ce878db7c9c756a13640cb0b0
Shohanojjaman <shohanojjamanemon@gmail.com>
2023-09-17 16:40:04 +0600

    Added Bookmarks components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Blogs from './components/Blog/Blogs';
+import Bookmarks from './components/Bookmark/Bookmarks';
 import Header from './components/Header';
 import './index.css';
 
@@ -8,7 +9,7 @@ function App() {
       <Header></Header>
       <main className="container mx-auto max-sm:px-5 flex">
         <Blogs></Blogs>
-        <div className="w-1/3"></div>
+        <Bookmarks ></Bookmarks>
       </main>
     </>
   );
---
+++ src/components/Bookmark/Bookmarks.jsx
@@ -0,0 +1,13 @@
+import PropTypes from 'prop-types';
+
+const Bookmarks = () => {
+    return (<div className="w-1/3">
+        <h4>Bookmarks</h4>
+    </div>)
+};
+
+Bookmarks.propTypes = {
+    
+};
+
+export default Bookmarks;
