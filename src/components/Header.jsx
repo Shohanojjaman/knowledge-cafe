1c91819aa871cd374232b1ce2ed95213de91c4aa
Shohanojjaman <shohanojjamanemon@gmail.com>
2023-09-17 17:42:47 +0600

    Fixed header padding issue.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import '../index.css';
 
 const Header = () => {
   return (
-    <header className="container mx-auto max-sm:px-4 flex justify-between items-center mb-8 border-0 border-b border-[#11111126] border-solid">
+    <header className="container mx-auto max-sm:px-4 flex justify-between items-center py-8 mb-8 border-0 border-b border-[#11111126] border-solid">
       <h1 className="text-4xl font-bold">Knowledge Cafe</h1>
       <img className="h-14" src={profile} alt="" />
     </header>
