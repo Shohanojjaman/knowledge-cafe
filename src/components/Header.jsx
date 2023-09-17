e4cd7bf297c5986bf28cd01acf3e5d374630be6f
Shohanojjaman <shohanojjamanemon@gmail.com>
2023-09-17 15:38:22 +0600

    Change Profile height.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className="container mx-auto max-sm:px-4 flex justify-between items-center mb-8 border-0 border-b border-[#11111126] border-solid">
       <h1 className="text-4xl font-bold">Knowledge Cafe</h1>
-      <img className="h-full" src={profile} alt="" />
+      <img className="h-14" src={profile} alt="" />
     </header>
   );
 };
