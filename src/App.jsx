026171b401f8658d281d5ed5e9a6977cd3863f02
Shohanojjaman <shohanojjamanemon@gmail.com>
2023-09-17 16:33:42 +0600

    Added Blog components which show all data.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Blogs from './components/Blog/Blogs';
 import Header from './components/Header';
 import './index.css';
 
@@ -5,7 +6,10 @@ function App() {
   return (
     <>
       <Header></Header>
-
+      <main className="container mx-auto max-sm:px-5 flex">
+        <Blogs></Blogs>
+        <div className="w-1/3"></div>
+      </main>
     </>
   );
 }
---
+++ src/components/Blog/Blog.jsx
@@ -0,0 +1,57 @@
+import PropTypes from 'prop-types';
+
+const Blog = ({ blog }) => {
+  const { title, cover, postDate, hashtags, author, authorImage, readingTime } = blog;
+
+  return (
+    <>
+      <div className="space-y-6">
+        <img src={cover} alt={`Image of ${title}`} className="w-full rounded-lg" />
+        <div className="flex justify-between items-center">
+          <div className="flex gap-6 ">
+            <img className="h-14" src={authorImage} alt={`Image of ${author}`} />
+            <div>
+              <h5 className="text-header text-2xl">{author}</h5>
+              <p className="text-body text-base">{postDate}</p>
+            </div>
+          </div>
+          <div className="flex items-center gap-2">
+            <p className="text-body">0{readingTime} min read</p>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+              <path
+                d="M17.593 3.322C18.693 3.45 19.5 4.399 19.5 5.507V21L12 17.25L4.5 21V5.507C4.5 4.399 5.306 3.45 6.407 3.322C10.1232 2.89063 13.8768 2.89063 17.593 3.322Z"
+                stroke="#111111"
+                strokeOpacity="0.6"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </div>
+        </div>
+        <h3 className="text-4xl text-header">{title}</h3>
+        <div>
+          {hashtags.map((tag, index) => (
+            <span className="mr-4" key={index}>
+              <a className="text-body font-medium" href={tag}>
+                {tag}
+              </a>
+            </span>
+          ))}
+        </div>
+        <p>
+          <a className="text-xl font-medium" href="#">
+            Mark as read
+          </a>
+        </p>
+      </div>
+      <hr className="bg-[#1111111A] h-[1px] border-none my-8" />
+    </>
+  );
+};
+
+Blog.propTypes = {
+  blog: PropTypes.object.isRequired,
+};
+
+export default Blog;
